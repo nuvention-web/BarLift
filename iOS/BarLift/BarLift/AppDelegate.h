8d5b0abd29fd0469d23f16d645384e2d9a588de0
Shikhar Mohan <shikharmohan2016@u.northwestern.edu>
2015-01-16 11:56:50 -0600

    initialized ios app

---
+++ iOS/BarLift/BarLift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BarLift
+//
+//  Created by Shikhar Mohan on 1/16/15.
+//  Copyright (c) 2015 Shikhar Mohan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
